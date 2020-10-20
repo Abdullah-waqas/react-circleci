37c3793db608eb20bc42f9e39cf9b094b4104f8e
abdullah-waqas <dev.waqas08@gmail.com>
2020-10-20 21:49:42 +0500

    fixed coverage issue

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import TodoList from "./TodoList";
+import TodoList from "./components/TodoList";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoList />
-    
+
   </React.StrictMode>,
   document.getElementById("root")
 );
